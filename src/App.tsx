9325cbbd52baec47e06534494a5703a9bc0955e2
ayman <aymansaud67@gmail.com>
2025-12-06 02:52:04 +0300

    Responsive style and demo ProductCard style

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import ProductCard from "./components/ProductCard";
 const App = () => {
-  return (<div><ProductCard /></div>);
+  return (<div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 ">
+    <ProductCard />
+    </div>);
 };
 
 export default App;
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -2,7 +2,31 @@ interface Iprops {
 
 }
 const ProductCard = ({}: Iprops) => {
-  return (<div>component</div>);
+  return (
+  <div className="border p-2  rounded-md flex flex-col">
+    <div className="flex flex-col">
+      <img src="https://images-cdn.ubuy.co.in/6613cb75a323b407bd014d6e-kaoayi-kids-clothing-for-boy-spring.jpg" alt="product clothes" />
+      <h3>Title</h3>
+    </div>
+    <div>
+      <p>hello this is description</p>
+    </div>
+    <div className="flex items-center space-x-1 mt-2">
+      <span className="w-3 h-3  bg-black rounded-full cursor-pointer"/>
+      <span className="w-3 h-3  bg-red-700 rounded-full cursor-pointer"/>
+      <span className="w-3 h-3  bg-yellow-500 rounded-full cursor-pointer"/>
+    </div>
+    <div className="flex items-center space-x-1 justify-between mt-2">
+      <span>100.00$</span>
+      <img className="flex w-10 h-10 rounded-full " src="https://images-cdn.ubuy.co.in/6613cb75a323b407bd014d6e-kaoayi-kids-clothing-for-boy-spring.jpg" alt="product clothes" />
+    </div>
+    <div className="flex space-x-1 mt-2">
+      <button className="bg-blue-500 w-full rounded-md border-2 p-2 border-black cursor-pointer">Edit</button>
+      <button className="bg-red-500 w-full rounded-md border-2 p-2 border-black cursor-pointer">Delete</button>
+      </div> 
+  </div>
+
+);
 };
 
 export default ProductCard;
